91ccb4d15a13dc46b513f72bc91c615db6298707
chaw <d.chaow@gmail.com>
2024-10-03 17:51:02 +0900

    Initial commit

---
+++ src/main/java/com/chaw/hhplus_tdd_lecture/HhplusTddLectureApplication.java
@@ -0,0 +1,13 @@
+package com.chaw.hhplus_tdd_lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusTddLectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhplusTddLectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chaw/hhplus_tdd_lecture/HhplusTddLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaw.hhplus_tdd_lecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusTddLectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
